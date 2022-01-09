83b1248cdfd1853721ba5b010488fff8acf9d2ab
Jacob Elias <jacobelias.gv@gmail.com>
2022-01-08 23:24:36 -0600

    Succesfully getting and printing multiple assets rates

--- src/load/load-aave.go
+++ src/load/load-aave.go
@@ -2,128 +2,23 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"math"
-	"math/big"
-	"os"
-	"strconv"
-
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/jelias2/go-eth-hello-world/src/decimal"
 	"github.com/jelias2/go-eth-hello-world/src/external-contracts/aave/AaveProtocolDataProvider"
 	"github.com/jelias2/go-eth-hello-world/src/external-contracts/aave/ILendingPool"
 	"github.com/jelias2/go-eth-hello-world/src/external-contracts/aave/ILendingPoolAddressProvider"
 	"github.com/jelias2/go-eth-hello-world/src/structs"
+	"log"
+	"os"
 )
 
 const (
-	secPerYear                        float64 = 31536000
-	LendingPoolAddressProviderAddress         = "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5"
-	USDT_MAINNET_ADDRESS                      = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
-	TUSD_MAINNET_ASSET_ADDRESS                = "0x0000000000085d4780B73119b644AE5ecd22b376"
+	LendingPoolAddressProviderAddress = "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5"
+	USDT_MAINNET_ADDRESS              = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
+	TUSD_MAINNET_ASSET_ADDRESS        = "0x0000000000085d4780B73119b644AE5ecd22b376"
+	DAI_MAINNET_ASSET_ADDRESS         = "0x6b175474e89094c44da98b954eedeac495271d0f"
+	USDC_MAINNET_ASSET_ADDRESS        = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
 )
-
-func logAccountStruct(input structs.AaveAccount) {
-	// No decimals in solditiy everything needs to be moved by 18 decimal places
-	eighteen := decimal.NewDec(1e18)
-
-	fmt.Printf("\n-----\tAccount Dump\t-----\n\t TotalCollateralETH: %v \n\t TotalDebtETH: %v \n\t AvailableBorrowsETH: %v \n\t CurrentLiquidationThreshold: %v \n\t Loan To Value: %v \n\t HealthFactor: %v \n ---------------------------\n",
-		decimal.NewDecFromBigInt(input.TotalCollateralETH).Quo(eighteen).String(),
-		decimal.NewDecFromBigInt(input.TotalDebtETH).Quo(eighteen).String(),
-		decimal.NewDecFromBigInt(input.AvailableBorrowsETH).Quo(eighteen).String(),
-		decimal.NewDecFromBigInt(input.CurrentLiquidationThreshold).Quo(eighteen).String(),
-		decimal.NewDecFromBigInt(input.Ltv).Quo(eighteen).String(),
-		decimal.NewDecFromBigInt(input.HealthFactor).Quo(eighteen).String(),
-	)
-}
-
-func rayValueToPercentage(rayValue *big.Int) string {
-	var ray big.Int
-	// This ray is only 25 positions, 27 give decimals APR, 25 give percentage -> 0.02 = 2%
-	ray.SetString("10000000000000000000000000", 10)
-	twentyfive := decimal.NewDecFromBigInt(&ray)
-	return decimal.NewDecFromBigInt(rayValue).Quo(twentyfive).String()
-}
-
-// Yes ugly, first get it to work, then get it pretty, premature optimzation is the root of all evil
-func stringPercentageToFloat(percentage string) float64 {
-	val, err := strconv.ParseFloat(percentage, 64)
-	if err != nil {
-		fmt.Printf("Error converting string percentage to float: %s \n", err.Error())
-	}
-	//fmt.Printf("Succuessfully converted %s -> %f \n", percentage, val)
-	return val
-}
-
-/*
-https://docs.aave.com/developers/guides/apy-and-apr
-
-  depositAPR = liquidityRate/RAY
-  variableBorrowAPR = variableBorrowRate/RAY
-  stableBorrowAPR = variableBorrowRate/ray
-
-  depositAPY = ((1 + (depositAPR / SECONDS_PER_YEAR)) ^ SECONDS_PER_YEAR) - 1
-  variableBorrowAPY = ((1 + (variableBorrowAPR / SECONDS_PER_YEAR)) ^ SECONDS_PER_YEAR) - 1
-  stableBorrowAPY = ((1 + (stableBorrowAPR / SECONDS_PER_YEAR)) ^ SECONDS_PER_YEAR) - 1
-
-  APY = ((1 + (APR / SECONDS_PER_YEAR)) ^ SECONDS_PER_YEAR) - 1
-*/
-
-func aprToAPY(inputAPR *big.Int) float64 {
-	aprString := rayValueToPercentage(inputAPR)
-	aprFloat := stringPercentageToFloat(aprString) / 100
-	apy := math.Pow((1+(aprFloat/secPerYear)), secPerYear) - 1
-	return apy
-
-}
-
-func logLendingPoolStruct(input struct {
-	AvailableLiquidity      *big.Int
-	TotalStableDebt         *big.Int
-	TotalVariableDebt       *big.Int
-	LiquidityRate           *big.Int
-	VariableBorrowRate      *big.Int
-	StableBorrowRate        *big.Int
-	AverageStableBorrowRate *big.Int
-	LiquidityIndex          *big.Int
-	VariableBorrowIndex     *big.Int
-	LastUpdateTimestamp     *big.Int
-}) {
-	// https://docs.aave.com/developers/the-core-protocol/lendingpool#getreservedata
-	// data is returned in ray units = 10^27
-	// No decimals in solditiy everything needs to be moved by 18 decimal places
-	eighteen := decimal.NewDec(1e18)
-	//RAY := math.Pow(10, 27) // 10 to the power 27
-
-	fmt.Printf("\n-----\tReserve Data Dump\t-----\n\t"+
-		"AvailableLiquidity: %v \n\t"+
-		"TotalStableDebt: %v \n\t"+
-		"TotalVariableDebt: %v \n\t"+
-		"LiquidityRate: %v \n\t"+
-		"AverageStableBorrowRate: %v \n\n\t"+
-		"Stable Borrow APR: %v \n\t"+
-		"Stable Borrow APY: %.2f \n\n\t"+
-		"Variable Borrow APR: %v \n\t"+
-		"Variable Borrow APY: %.2f \n\n\t"+
-		"Deposit APR: %v \n\t"+
-		"Deposit APY: %.2f \n\t"+
-		"LastUpdateTimestamp: %v \n"+
-		"---------------------------\n",
-		input.AvailableLiquidity,
-		decimal.NewDecFromBigInt(input.TotalStableDebt).Quo(eighteen).String(),
-		decimal.NewDecFromBigInt(input.TotalVariableDebt).Quo(eighteen).String(),
-		input.LiquidityRate,
-		input.AverageStableBorrowRate,
-		rayValueToPercentage(input.StableBorrowRate),
-		aprToAPY(input.StableBorrowRate)*100,
-		rayValueToPercentage(input.VariableBorrowRate),
-		aprToAPY(input.VariableBorrowRate)*100,
-		rayValueToPercentage(input.LiquidityRate),
-		aprToAPY(input.LiquidityRate)*100,
-		decimal.NewDecFromBigInt(input.LastUpdateTimestamp).Quo(eighteen).String(),
-	)
-}
 
 func main() {
 
@@ -135,6 +30,15 @@ func main() {
 		fmt.Println("exiting...")
 		os.Exit(0)
 	}
+
+	var addressMap = map[string]string{
+		"USDT": USDT_MAINNET_ADDRESS,
+		"TUSD": TUSD_MAINNET_ASSET_ADDRESS,
+		"DAI":  DAI_MAINNET_ASSET_ADDRESS,
+		"USDC": USDC_MAINNET_ASSET_ADDRESS,
+	}
+
+	lendingPoolMap := make(map[string]structs.WrappedLendingPool)
 
 	client, err := ethclient.Dial(endpoint)
 	if err != nil {
@@ -189,12 +93,12 @@ func main() {
 	// log the account data
 	aaveAccount.LogAccountStruct()
 
-	addressProvider, err := lendingPoolCaller.GetAddressesProvider(nil)
-	if err != nil {
-		log.Print("Error getting LendingPool Address!")
-		log.Fatal(err)
-	}
-	fmt.Printf("Address Provider: %v \n", addressProvider)
+	// addressProvider, err := lendingPoolCaller.GetAddressesProvider(nil)
+	// if err != nil {
+	// 	log.Print("Error getting LendingPool Address!")
+	// 	log.Fatal(err)
+	// }
+	//fmt.Printf("Address Provider: %v \n", addressProvider)
 
 	//Create the Aave Protocol Data Provider, not sure if the aave_account_address is the proper one to pass in
 	aave_data_provider_caller, err := AaveProtocolDataProvider.NewAaveProtocolDataProviderCaller(lendingPoolAddress, client)
@@ -203,34 +107,14 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// https://docs.aave.com/developers/the-core-protocol/lendingpool#getreservedata
-	// data is returned in ray units = 10^27
-	reserve_data, err := aave_data_provider_caller.GetReserveData(nil, common.HexToAddress(TUSD_MAINNET_ASSET_ADDRESS))
-	if err != nil {
-		log.Print("Error getting token data!")
-		log.Fatal(err)
+	for ticker, addr := range addressMap {
+		rawReserveData, err := aave_data_provider_caller.GetReserveData(nil, common.HexToAddress(addr))
+		if err != nil {
+			log.Print("Error getting token data!")
+			log.Fatal(err)
+		}
+		wrappedLendingPool := structs.NewWrappedLendingPool(rawReserveData, ticker, addr)
+		lendingPoolMap[ticker] = wrappedLendingPool
 	}
-
-	logLendingPoolStruct(reserve_data)
+	structs.SortedDepositRates(lendingPoolMap)
 }
-
-// 	for index, token := range token_data {
-
-// 		fmt.Print("\n %d. Name: %s Address: %s", index, token.Symbol, token.TokenAddress)
-// 	}
-
-// 	marketID, err := aave_instance.GetMarketId(nil)
-// 	if err != nil {
-// 		log.Print("Error calling getMarketID!")
-// 		log.Fatal(err)
-// 	}
-
-// 	fmt.Printf("Current MarketID: %v \n", marketID)
-
-// 	marketID, err := aave_instance.G(nil)
-//   if err != nil {
-//     log.Print("Error calling TotalBorrows!")
-//     log.Fatal(err)
-//   }
-
-//   fmt.Printf("Current Compound is CDai Total Supply: %v \n", TotalBorrows)
--- src/structs/aave_structs.go
+++ src/structs/aave_structs.go
@@ -3,13 +3,22 @@ package structs
 import (
 	"fmt"
 	"github.com/jelias2/go-eth-hello-world/src/decimal"
+	"math"
 	"math/big"
+	"sort"
+	"strconv"
 )
 
 var (
 	// No decimals in solditiy everything needs to be moved by 18 decimal places
-	eighteen = decimal.NewDec(1e18)
+	eighteen           = decimal.NewDec(1e18)
+	secPerYear float64 = 31536000
 )
+
+type AssetAddress struct {
+	Ticker  string
+	Address string
+}
 
 type AaveAccount struct {
 	TotalCollateralETH          *big.Int
@@ -48,4 +57,162 @@ func (a *AaveAccount) LogAccountStruct() {
 		decimal.NewDecFromBigInt(a.Ltv).Quo(eighteen).String(),
 		decimal.NewDecFromBigInt(a.HealthFactor).Quo(eighteen).String(),
 	)
+}
+
+type Rates struct {
+	StableBorrowAPR   float64
+	StableBorrowAPY   float64
+	VariableBorrowAPR float64
+	VariableBorrowAPY float64
+	DepositAPR        float64
+	DepositAPY        float64
+}
+
+type WrappedLendingPool struct {
+	Ticker                  string
+	AvailableLiquidity      *big.Int
+	TotalStableDebt         *big.Int
+	TotalVariableDebt       *big.Int
+	LiquidityRate           *big.Int
+	VariableBorrowRate      *big.Int
+	StableBorrowRate        *big.Int
+	AverageStableBorrowRate *big.Int
+	LiquidityIndex          *big.Int
+	VariableBorrowIndex     *big.Int
+	LastUpdateTimestamp     *big.Int
+	URL                     string
+	Rates                   Rates
+}
+
+func NewWrappedLendingPool(input struct {
+	AvailableLiquidity      *big.Int
+	TotalStableDebt         *big.Int
+	TotalVariableDebt       *big.Int
+	LiquidityRate           *big.Int
+	VariableBorrowRate      *big.Int
+	StableBorrowRate        *big.Int
+	AverageStableBorrowRate *big.Int
+	LiquidityIndex          *big.Int
+	VariableBorrowIndex     *big.Int
+	LastUpdateTimestamp     *big.Int
+}, ticker, address string) WrappedLendingPool {
+	return WrappedLendingPool{
+		Ticker:                  ticker,
+		AvailableLiquidity:      input.AvailableLiquidity,
+		TotalStableDebt:         input.TotalStableDebt,
+		TotalVariableDebt:       input.TotalVariableDebt,
+		LiquidityRate:           input.LiquidityRate,
+		VariableBorrowRate:      input.VariableBorrowRate,
+		StableBorrowRate:        input.StableBorrowRate,
+		AverageStableBorrowRate: input.AverageStableBorrowRate,
+		LiquidityIndex:          input.LiquidityIndex,
+		VariableBorrowIndex:     input.VariableBorrowIndex,
+		LastUpdateTimestamp:     input.LastUpdateTimestamp,
+		URL:                     fmt.Sprintf("https://app.aave.com/#/reserve-overview/%s", address),
+		Rates: Rates{
+			StableBorrowAPR:   rayValueToPercentage(input.StableBorrowRate),
+			StableBorrowAPY:   aprToAPY(input.StableBorrowRate),
+			VariableBorrowAPR: rayValueToPercentage(input.VariableBorrowRate),
+			VariableBorrowAPY: aprToAPY(input.VariableBorrowRate),
+			DepositAPR:        rayValueToPercentage(input.LiquidityRate),
+			DepositAPY:        aprToAPY(input.LiquidityRate),
+		},
+	}
+}
+
+// Create a list type of WrappedLendingPools with length and custom sorting function (by deposit APR) for that list
+type WrappedLendingPoolList []WrappedLendingPool
+
+func (wllpl WrappedLendingPoolList) Len() int      { return len(wllpl) }
+func (wllpl WrappedLendingPoolList) Swap(i, j int) { wllpl[i], wllpl[j] = wllpl[j], wllpl[i] }
+func (wllpl WrappedLendingPoolList) Less(i, j int) bool {
+	return wllpl[i].Rates.DepositAPR < wllpl[j].Rates.DepositAPR
+}
+
+// SortedDepositRates will take in the LP map and output a list ordered by the best deposit intrest rates
+func SortedDepositRates(lpMap map[string]WrappedLendingPool) {
+
+	var wllpList []WrappedLendingPool
+
+	for _, val := range lpMap {
+		wllpList = append(wllpList, val)
+	}
+
+	sort.Sort(sort.Reverse(WrappedLendingPoolList(wllpList)))
+	fmt.Println("\n----- Sorted Deposit Rates -----")
+	for _, lp := range wllpList {
+		fmt.Printf("\tTicker: %s DepositAPR: %.3f \n", lp.Ticker, lp.Rates.DepositAPR)
+	}
+	fmt.Println("\n-------------------------------")
+}
+
+func (w *WrappedLendingPool) LogLendingPoolStruct() {
+
+	fmt.Printf("\n-----\t %s Reserve Data Dump\t-----\n\t"+
+		"AvailableLiquidity: %v \n\t"+
+		"TotalStableDebt: %v \n\t"+
+		"TotalVariableDebt: %v \n\t"+
+		"LiquidityRate: %v \n\t"+
+		"AverageStableBorrowRate: %v \n\n\t"+
+		"Stable Borrow APY: %.3f \n\t"+
+		"Stable Borrow APR: %.3f \n\n\t"+
+		"Variable Borrow APY: %.3f \n\t"+
+		"Variable Borrow APR: %.3f \n\n\t"+
+		"Deposit APY: %.3f \n\t"+
+		"Deposit APR: %.3f \n\n\t"+
+		"LastUpdateTimestamp: %v \n\t"+
+		"URL: %s \n"+
+		"---------------------------\n",
+		w.Ticker,
+		w.AvailableLiquidity,
+		decimal.NewDecFromBigInt(w.TotalStableDebt).Quo(eighteen).String(),
+		decimal.NewDecFromBigInt(w.TotalVariableDebt).Quo(eighteen).String(),
+		w.LiquidityRate,
+		w.AverageStableBorrowRate,
+		w.Rates.StableBorrowAPY,
+		w.Rates.StableBorrowAPR,
+		w.Rates.VariableBorrowAPY,
+		w.Rates.VariableBorrowAPR,
+		w.Rates.DepositAPY,
+		w.Rates.DepositAPR,
+		decimal.NewDecFromBigInt(w.LastUpdateTimestamp).Quo(eighteen).String(),
+		w.URL,
+	)
+}
+
+func rayValueToPercentage(rayValue *big.Int) float64 {
+	var ray big.Int
+	// This ray is only 25 positions, 27 give decimals APR, 25 give percentage -> 0.02 = 2%
+	ray.SetString("10000000000000000000000000", 10)
+	twentyfive := decimal.NewDecFromBigInt(&ray)
+	return stringPercentageToFloat(decimal.NewDecFromBigInt(rayValue).Quo(twentyfive).String())
+}
+
+// Yes ugly, first get it to work, then get it pretty, premature optimzation is the root of all evil
+func stringPercentageToFloat(percentage string) float64 {
+	val, err := strconv.ParseFloat(percentage, 64)
+	if err != nil {
+		fmt.Printf("Error converting string percentage to float: %s \n", err.Error())
+	}
+	return val
+}
+
+/*
+https://docs.aave.com/developers/guides/apy-and-apr
+
+  depositAPR = liquidityRate/RAY
+  variableBorrowAPR = variableBorrowRate/RAY
+  stableBorrowAPR = variableBorrowRate/ray
+
+  depositAPY = ((1 + (depositAPR / SECONDS_PER_YEAR)) ^ SECONDS_PER_YEAR) - 1
+  variableBorrowAPY = ((1 + (variableBorrowAPR / SECONDS_PER_YEAR)) ^ SECONDS_PER_YEAR) - 1
+  stableBorrowAPY = ((1 + (stableBorrowAPR / SECONDS_PER_YEAR)) ^ SECONDS_PER_YEAR) - 1
+
+  APY = ((1 + (APR / SECONDS_PER_YEAR)) ^ SECONDS_PER_YEAR) - 1
+*/
+
+func aprToAPY(inputAPR *big.Int) float64 {
+	apr := rayValueToPercentage(inputAPR) / 100
+	apy := math.Pow((1+(apr/secPerYear)), secPerYear) - 1
+	return apy * 100
 }
